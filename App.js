4e3f123984d5215129cf9db3220c52180b0d5ecf
Sinval Felisberto <sinvalfelisberto@gmail.com>
2020-10-02 12:42:01 -0300

    teste

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 export default function App() {
   return(
@@ -8,3 +8,10 @@ export default function App() {
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: #E4401D
+  }
+})
